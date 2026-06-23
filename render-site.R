fc48deed0bb91e578078bf38ebd2dbc55fa2fb45
unknown <s.bogatsu18@hotmail.com>
2026-06-23 20:54:09 +0200

    ix Stata collectcode and rebuild site

--- render-site.R
+++ render-site.R
@@ -5,6 +5,12 @@ if (!file.exists("_bookdown.yml")) {
   stop("Run this script from the pasec-guide project root.")
 }
 
+# Remove stale knitr/bookdown cache (can cause empty Stata output)
+for (d in c("bookdownproj_cache", "_bookdown_files")) {
+  if (dir.exists(d)) unlink(d, recursive = TRUE)
+}
+if (file.exists("profile.do")) unlink("profile.do")
+
 bookdown::render_book("index.Rmd")
 source("R/publish-nav.R", local = TRUE)
 
